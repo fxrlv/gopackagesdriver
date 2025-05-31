08de4ebe90c1f20d43a765bbb0e7aaeaa542797d
Aleksandr Frolov <fxrlv@nebius.com>
2025-05-31 22:55:36 +0200

    driver init

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"golang.org/x/tools/go/packages"
+)
+
+func main() {
+	var req packages.DriverRequest
+
+	err := json.NewDecoder(os.Stdin).Decode(&req)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	workdir, err := os.Getwd()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	config := DriverConfig{
+		Workspace: os.Getenv("GOPACKAGESDRIVER_WORKSPACE"),
+		Bazel:     os.Getenv("GOPACKAGESDRIVER_BAZEL"),
+		Workdir:   workdir,
+	}
+
+	driver, err := NewDriver(&config)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	resp, err := driver.Serve(&req, os.Args[1:])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	err = json.NewEncoder(os.Stdout).Encode(resp)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
