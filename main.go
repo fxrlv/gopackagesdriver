6fcf240e2bbc7172802c91c4342e440d3ad2923b
Alexander Frolov <fxrlv@icloud.com>
2025-07-06 22:48:28 +0200

    feat: add overlay command-line interface
    Add -init and -sync flags for overlay file management:
    - MakeOverlay() method for creating/updating overlay.json files
    - Refactored main() to support CLI commands alongside driver mode

--- main.go
+++ main.go
@@ -2,20 +2,20 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"os"
 
 	"golang.org/x/tools/go/packages"
 )
 
-func main() {
-	var req packages.DriverRequest
+var (
+	initOverlay = flag.Bool("init", false, "init overlay")
+	syncOverlay = flag.Bool("sync", false, "sync overlay")
+)
 
-	err := json.NewDecoder(os.Stdin).Decode(&req)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
+func main() {
+	flag.Parse()
 
 	workdir, err := os.Getwd()
 	if err != nil {
@@ -35,15 +35,35 @@ func main() {
 		os.Exit(1)
 	}
 
-	resp, err := driver.Serve(&req, os.Args[1:])
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+	if *initOverlay || *syncOverlay {
+		path, err := driver.MakeOverlay(*syncOverlay)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		fmt.Println(path)
+		return
 	}
 
-	err = json.NewEncoder(os.Stdout).Encode(resp)
+	err = serve(driver, flag.Args())
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+}
+
+func serve(d *Driver, args []string) error {
+	var req packages.DriverRequest
+
+	err := json.NewDecoder(os.Stdin).Decode(&req)
+	if err != nil {
+		return err
+	}
+
+	resp, err := d.Serve(&req, args)
+	if err != nil {
+		return err
+	}
+
+	return json.NewEncoder(os.Stdout).Encode(resp)
 }
